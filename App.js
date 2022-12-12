41ef9c4ed111e43d98f689cd19ac43053ee15ad6
Eric Polley <109182529+ericpolley@users.noreply.github.com>
2022-12-12 08:29:19 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './App.css';
+import Blog001 from './Blogs/Blog001';
+import Blog002 from './Blogs/Blog002';
+import Blog003 from './Blogs/Blog003';
+import Blog004 from './Blogs/Blog004';
+import Blog005 from './Blogs/Blog005';
+import Blog006 from './Blogs/Blog006';
+import Blog007 from './Blogs/Blog007';
+import Blog008 from './Blogs/Blog008';
+import Blog009 from './Blogs/Blog009';
+import Blog010 from './Blogs/Blog010';
+import Blog011 from './Blogs/Blog011';
+import Blog012 from './Blogs/Blog012';
+import Blog013 from './Blogs/Blog013';
+import Blog014 from './Blogs/Blog014';
+import Blog015 from './Blogs/Blog015';
+import Blog016 from './Blogs/Blog016';
+import Blog017 from './Blogs/Blog017';
+import Blog018 from './Blogs/Blog018';
+import Blog019 from './Blogs/Blog019';
+import Blog020 from './Blogs/Blog020';
+import Blog021 from './Blogs/Blog021';
+import Blog022 from './Blogs/Blog022';
+import Blog023 from './Blogs/Blog023';
+import Blog024 from './Blogs/Blog024';
+import Blog025 from './Blogs/Blog025';
+import Blog026 from './Blogs/Blog026';
+import Blog027 from './Blogs/Blog027';
+
+
+function App() {
+  return (
+    <>
+    <div className="nav">
+     <div className="navbar">
+     
+    <h1>Eric Polley - Official Blog</h1>
+    <a target="blank" href="http://ericpolley.com">
+    <p>A technical Blog by Eric Polley </p>
+    </a>
+    </div></div>
+    <Blog027 />
+    <Blog026 />
+    <Blog025 />
+    <Blog024 />
+    <Blog023 />
+    <Blog022 />
+    <Blog021 />
+    <Blog020 />
+    <Blog019 />
+    <Blog018 />
+    <Blog017 />
+    <Blog016 />
+    <Blog015 />
+    <Blog014 />
+    <Blog013 />
+    <Blog012 />
+    <Blog011 />
+    <Blog010 />
+    <Blog009 />
+    <Blog008 />
+    <Blog007 />
+    <Blog006 />
+    <Blog005 />
+    <Blog004 />
+    <Blog003 />
+    <Blog002 />
+    <Blog001 />
+   
+        
+       
+        
+    
+        
+      
+      
+
+    </>
+  );
+}
+
+export default App;
